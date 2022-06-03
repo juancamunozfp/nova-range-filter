5de746fe49610849695cc83d0b10a5813d60e51c
Juan Carlos Muñoz Cabrera <jcmunoz@freepik.com>
2022-06-03 11:22:57 +0200

    nova 4

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,9 @@
 let mix = require('laravel-mix')
 
-mix.setPublicPath('dist')
+require('./nova.mix.js');
+
+mix
+    .setPublicPath('dist')
     .js('resources/js/filter.js', 'js')
+    .vue({ version: 3 })
     .sass('resources/sass/filter.scss', 'css')
